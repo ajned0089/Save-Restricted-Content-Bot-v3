ac1be2427a2c20f5a094b159eb5034c3b1025d8a
ajned0089 <ammarjned71@gmail.com>
2025-08-22 08:10:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -24,8 +24,8 @@ MONGO_DB = os.getenv("MONGO_DB", "")
 OWNER_ID = list(map(int, os.getenv("OWNER_ID", "7742356911").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
 STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "")) # optional with -100
-FORCE_SUB = int(os.getenv("FORCE_SUB", "")) # optional with -100
+LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002939122641")) # optional with -100
+FORCE_SUB = int(os.getenv("FORCE_SUB", "-1003074196403")) # optional with -100
 MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
 IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
 YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
@@ -34,5 +34,6 @@ FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
 JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/team_spy_pro") # this link for start command message
 ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/username_of_admin")
+
 
 
